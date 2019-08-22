1bb52d617de5d6c0550eb5c0ca06c676b6141e20
David Banks <amoebae@gmail.com>
2019-08-22 16:05:06 +0100

    trying to get type safety

--- src/main/resources/assets/src/components/graph.ts
+++ src/main/resources/assets/src/components/graph.ts
@@ -1,19 +1,40 @@
-import { mountGraphView } from '../graph-view-embed';
+// import { mountGraphView } from '../graph-view-embed';
 
-const graph = {
+import { TokenTreeNode } from 'occubrow-graph-view';
+
+interface IController {
+    $onInit?(): void;
+    $doCheck?(): void;
+    $onChanges?(onChangesObj: ng.IOnChangesObject): void;
+    $onDestroy?(): void;
+    $postLink?(): void;
+}
+
+interface GraphDataContainer {
+    isLoaded: boolean;
+    data: TokenTreeNode | null;
+}
+
+interface GraphController extends IController {
+    graphData: any;
+};
+
+const graph: ng.IComponentOptions = {
     templateUrl: 'html/components/graph.html',
     bindings: {
     },
     controller: function($http: ng.IHttpService) {
-        console.log("inside controller");
-        const $ctrl = this;
+        const $ctrl = this as GraphController;
+        console.log("inside controller with values %o", this);
 
-        $ctrl.graphData = {
-            isLoaded: false,
-            data: null
-        };
+        $ctrl.graphData = 42;
 
-        this.$onInit = function() {
+        // $ctrl.graphData = {
+        //     isLoaded: false,
+        //     data: null
+        // };
+
+        $ctrl.$onInit = function() {
             console.log("inside oninit", $http);
             $http({
                 method: 'GET',
@@ -26,11 +47,15 @@ const graph = {
             });
         };
 
-        this.$postLink = function() {
-            console.log("inside postlink");
-            console.log("view of graphdata inside postlink: ", $ctrl.graphData);
-            mountGraphView($ctrl.graphData);
+        $ctrl.$onInit = function() {
+            console.log("value of this inside onInit: %o", this);
         };
+
+        // this.$postLink = function() {
+        //     console.log("inside postlink");
+        //     console.log("view of graphdata inside postlink: ", $ctrl.graphData);
+        //     // mountGraphView($ctrl.graphData);
+        // };
     }
 };
 
--- src/main/resources/assets/src/graph-view-embed.ts
+++ src/main/resources/assets/src/graph-view-embed.ts
@@ -1,65 +1,65 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { GraphView, GraphViewModule } from 'occubrow-graph-view';
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+// import { GraphView, GraphViewModule } from 'occubrow-graph-view';
 
-const GRAPH_DATA = {
-    content: "Root",
-    id: 42,
-    label: "Blah",
-    strength: null,
-    children: []
-}
+// const GRAPH_DATA = {
+//     content: "Root",
+//     id: 42,
+//     label: "Blah",
+//     strength: null,
+//     children: []
+// }
 
-function makeProps(graphData) {
-    return {
-        width: 800,
-        height: 800,
-        xMargin: 0,
-        yMargin: 0,
-        depthOffset: 120,
-        textOffset: 22,
-        breadth: 360,
-        graphData: graphData,
-        textContentTemplate: "{{content}}"
-    }
-}
+// function makeProps(graphData) {
+//     return {
+//         width: 800,
+//         height: 800,
+//         xMargin: 0,
+//         yMargin: 0,
+//         depthOffset: 120,
+//         textOffset: 22,
+//         breadth: 360,
+//         graphData: graphData,
+//         textContentTemplate: "{{content}}"
+//     }
+// }
 
-function doTweak(e) {
-    console.log("I would tweak something");
-    GRAPH_DATA.content = "Something else";
+// function doTweak(e) {
+//     console.log("I would tweak something");
+//     GRAPH_DATA.content = "Something else";
 
-    GRAPH_DATA.children.push({
-        content: "Leaf",
-        id: 43,
-        label: "Blah3",
-        strength: null,
-        children: []
-    });
-}
-
-
-function mountGraphView(graphData) {
-    console.log("inside mountGraphView");
-
-    Vue.use(Vuex);
-
-    document.getElementById('tweak').addEventListener('click', doTweak);
-
-    const props = makeProps(graphData);
-
-    const store = new Vuex.Store({
-        modules: {
-            graphView: GraphViewModule
-        }
-    });
-
-    const vueInstance = new Vue({
-        store,
-        render: (h) => h(GraphView, { props })
-    });
-    vueInstance.$mount('#graph-target');
-}
+//     GRAPH_DATA.children.push({
+//         content: "Leaf",
+//         id: 43,
+//         label: "Blah3",
+//         strength: null,
+//         children: []
+//     });
+// }
 
 
+// function mountGraphView(graphData) {
+//     console.log("inside mountGraphView");
 
-export {mountGraphView};
+//     Vue.use(Vuex);
+
+//     document.getElementById('tweak').addEventListener('click', doTweak);
+
+//     const props = makeProps(graphData);
+
+//     const store = new Vuex.Store({
+//         modules: {
+//             graphView: GraphViewModule
+//         }
+//     });
+
+//     const vueInstance = new Vue({
+//         store,
+//         render: (h) => h(GraphView, { props })
+//     });
+//     vueInstance.$mount('#graph-target');
+// }
+
+
+
+// export {mountGraphView};
