168be22310fb76ca5c5ce79b60cb8fdffffb1245
David Banks <amoebae@gmail.com>
2019-08-22 12:20:13 +0100

    requesting graph api

--- src/main/resources/assets/src/components/graph.ts
+++ src/main/resources/assets/src/components/graph.ts
@@ -1,16 +1,28 @@
-import {mountGraphView} from '../graph-view-embed';
+import { mountGraphView } from '../graph-view-embed';
 
 const graph = {
     templateUrl: 'html/components/graph.html',
     bindings: {
     },
-    controller: function() {
+    controller: function($http: ng.IHttpService) {
         console.log("inside controller");
         const $ctrl = this;
-        
+
+
+        this.$onInit = function() {
+            console.log("inside oninit", $http);
+            $http({
+                method: 'GET',
+                url: '/api/graph'
+            }).then(response => {
+                console.log("success");
+            }).catch(response => {
+                console.log("failure");
+            });
+        };
+
         this.$postLink = function() {
             console.log("inside postlink");
-
             mountGraphView();
         };
     }
